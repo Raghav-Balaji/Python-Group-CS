43cfcf92445c5d01d2debec7962f5fa4b265ca66
Raghav <raghav.cb@gmail.com>
2023-12-11 22:28:22 +0530

    added comments explaining what i did

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-
+// so by clicking the signup or signin button the animation moves showing the intended forms for the clicked button
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
@@ -9,3 +9,5 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+// From here, i will add the google api manipulation code and try not to mess with it much as it is quite delicate + complex
