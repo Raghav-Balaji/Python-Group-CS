f1a99d3dadc52956888180bd162b60f552d1ae5c
GOUYSAKI <purujitsuresh@gmail.com>
2023-12-15 08:19:12 +0530

    Final list of all files

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 const login = document.getElementById('Login')
+const signupAccept = document.getElementById('SignupAccept')
+let DbPhoneNo = document.getElementById('DbPhoneNo')
+let DbPassword = document.getElementById('DbPassword')
 // so by clicking the signup or signin button the animation moves showing the intended forms for the clicked button
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -10,14 +13,23 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+DataBase={}
+function SaveDetails() {
+    database.DbPhoneNo=DbPassword;
+}
+
 
 function validateLogin() {
     // Check if the username and password match your database values
-    if (username === "your_username" && password === "your_password") {
+    for Dbusername in Database { 
+
+        if (username === "your_username" && password === "your_password") {
         // Redirect to the dashboard.html page
         window.location.href = "index.html";
-    } else {
+        } 
+    else {
         alert("Invalid username or password. Please try again.");
+        }
     }
 }
 
